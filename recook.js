6c1142b126f13eb2809cd53b89a0def9ca71c72d
Antonio Alvarado Hernández <tnotstar@gmail.com>
2020-10-25 19:28:49 +0100

    added support for logging
    - changed constructor signature to accept destructuring w/rest options
    - created a log method with test for logger option

--- recook.js
+++ recook.js
@@ -2,23 +2,25 @@
 
 class Recooker {
 
-  constructor(label, taste, cook) {
+  constructor({ label, taste, cook, ...options } = {}) {
     this.label = label || 'Recooker()'
     this.taste = taste || ($ => true)
     this.cook = cook || ($ => JSON.parse(JSON.stringify($) || null))
+    this.options = { ...options }
   }
 
-  assemble(label, ...args) {
+  assemble(options) {
     let last = this
     for (let next = last.next; next; next = last.next)
       last = next
-    let taste, cook
-    if (args.length > 1)
-      [ taste, cook ] = args
-    else if (args.length > 0)
-      [ cook ] = args
-    last.next = new Recooker(label, taste, cook)
+    last.next = new Recooker(options)
     return this
+  }
+
+  log(...args) {
+    const options = this.options
+    if (options.logger)
+      options.logger(...args)
   }
 
   toString() {
@@ -26,30 +28,39 @@ class Recooker {
   }
 
   recook($) {
-    let cooker = this
+    let cooker = this, i = 1
+    this.log('Recooking "%o" with cooker "%o"...', $, cooker)
     while (cooker) {
-      if (cooker.taste($)) $ = cooker.cook($)
+      const taste = cooker.taste($)
+      this.log('%i> After tasting "%o", he found it "%o"', i, $, taste)
+      if (taste) {
+        $ = cooker.cook($)
+        this.log('%i> After cooking it, now he has "%o"', i, $)
+      }
+      this.log('%i> Next step appears to be "%o"', i, cooker.next)
       cooker = cooker.next
+      i++
     }
+    this.log('Recooks has been finished with "%o"!', $)
     return $
   }
 
   set(name, _) {
-    return this.assemble(`set(${name}, ${_})`, ($) => {
+    return this.assemble({label:`set(${name}, ${_})`, cook:$ => {
       return Object.assign($ || {}, { [name]: 'function' === typeof _? _($): _ })
-    })
+    }})
   }
 
   rename(newName, oldName) {
-    return this.assemble(`rename(${newName}, ${oldName})`, $ => {
+    return this.assemble({label:`rename(${newName}, ${oldName})`, cook:$ => {
       if ('undefined' !== typeof $[oldName]) {
         $[newName] = $[oldName] && delete $[oldName]
       }
       return $
-    })
+    }})
   }
 }
 
-const preheat = () => new Recooker()
+const preheat = options => new Recooker(options)
 
 module.exports = { Recooker, preheat }
